ee4a6714e09b18dfa19115d33c36c4698f0d1278
Raskil <raskil2000@gmail.com>
2022-11-16 18:17:54 +0100

    Convertion of project files to sdk style projects.

--- ChatterThemepack/Properties/AssemblyInfo.cs
+++ ChatterThemepack/Properties/AssemblyInfo.cs
@@ -6,15 +6,3 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("98d43bae-1089-47eb-a7cf-70e2a1787a9b")]
 
-[assembly: AssemblyVersion("2.8.0.1")]
-[assembly: AssemblyFileVersion("2.8.0.1")]
-
-[assembly: AssemblyTitle("Chatter Themes extension pack")]
-[assembly: AssemblyDescription("Extension for VAICOM PRO plugin for DCS World")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VAICOM PRO Community Edition")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
--- VAICOM/Properties/AssemblyInfo.cs
+++ VAICOM/Properties/AssemblyInfo.cs
@@ -5,15 +5,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("30B9455A-E1AC-486C-AC4D-F1FD95588FBA")]
-
-[assembly: AssemblyVersion("2.8.0.1")]
-[assembly: AssemblyFileVersion("2.8.0.1")]
-
-[assembly: AssemblyTitle("VAICOM PRO Community Edition for DCS World")]
-[assembly: AssemblyDescription("Interactive Voice Communications for DCS World")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VAICOM PRO Community Edition")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
