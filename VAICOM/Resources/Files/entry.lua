fd9db78829459abd0103a2cd94c6bece1b766841
Penecruz <73371761+Penecruz@users.noreply.github.com>
2023-07-21 15:01:24 +1000

    Set For Next Release 2.8.5
    Minor fixes MB339 and F-5E-3
    Version changes
    Manual update
    Remove extensions folder from installer.

--- VAICOM/Resources/Files/entry.lua
+++ VAICOM/Resources/Files/entry.lua
@@ -10,7 +10,7 @@ declare_plugin(self_ID,
 	shortName	 = "VAICOM PRO",
 	fileMenuName = "VAICOM PRO Community Edition",
 
-	version		 = "2.8.3",
+	version		 = "2.8.5",
 	state		 = "installed", 	
 	developerName= "VAICOM Community",
 	info		 = _("VAICOM PRO Community Edition is a professional-grade voice communications interface. The plugin uses VoiceAttack as client host for advanced speech recognition, enabling true-to-life radio communications with all AI units in the mission."),
