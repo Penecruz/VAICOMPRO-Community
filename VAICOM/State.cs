dfd0d073ad5c83d55094fda73ede1f2fb9e35a77
Penecruz <73371761+Penecruz@users.noreply.github.com>
2023-12-21 06:36:15 +1100

    Beta 2.9.1.6 RC2
    - Added Paler Callsigns
    - Xmas logo

--- VAICOM/State.cs
+++ VAICOM/State.cs
@@ -35,7 +35,7 @@ namespace VAICOM
         public static string        clientmode          = ClientModes.Normal;
                                                                              
         public static string        versionstring       = "";
-        public static string        pluginversionnumber = "2.9.0"; // used by Theme (Special page)
+        public static string        pluginversionnumber = "2.9.1"; // used by Theme (Special page)
         public static string        vaminversion        = "1.10.6";
         public static string        defProfileName      = "VAICOM PRO for DCS World";
         public static bool          requirecarrierregkey= false;
